cc50212a2af010602727669686ecda5cbefb265e
Aman Kumar <amankumar21dec@gmail.com>
2024-08-24 09:59:47 +0530

    Update Ques 1.cpp
    Ques - 01 Write a C++ program that takes two strings as input from the user and concatenates them. Then, output the concatenated string.
    
    Sample Input :- Enter the first string: Hello
    Enter the second string: World
    
    Sample Output:- Concatenated string: HelloWorld

--- Day 1/Ques 1.cpp
+++ Day 1/Ques 1.cpp
@@ -1,10 +1,3 @@
-/*Ques - 01 Write a C++ program that takes two strings as input from the user and concatenates them. Then, output the concatenated string.
-
-Sample Input :- Enter the first string: Hello
-Enter the second string: World
-
-Sample Output:- Concatenated string: HelloWorld*/
-
 #include <iostream>
 using namespace std;
 int main(){
